1e417b59b9ecf6a24b4ca65e969ec1b6c98c47ca
Eren Taha ÇAĞŞAK <cagsakerentaha@gmail.com>
2026-01-22 11:28:43 +0300

    Add ProductService and IProductService interface
    Introduced the IProductService interface and its implementation, ProductService, to encapsulate product-related business logic. Registered ProductService in the dependency injection container in Program.cs. ProductService provides methods to retrieve all products and add a new product using DTOs.

--- Backend/ProductApi/Program.cs
+++ Backend/ProductApi/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using ProductApi.Data;
 using ProductApi.Repositories.Implementations;
 using ProductApi.Repositories.Interfaces;
+using ProductApi.Services.Interfaces;
+using ProductApi.Services.Implementations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
---
+++ Backend/ProductApi/Services/Implementations/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using ProductApi.DTOs;
+using ProductApi.Models;
+using ProductApi.Repositories.Interfaces;
+using ProductApi.Services.Interfaces;
+
+namespace ProductApi.Services.Implementations
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductService(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetAllAsync()
+        {
+            var products = await _repository.GetAllAsync();
+
+            return products.Select(p => new ProductDto
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Price = p.Price,
+                Stock = p.Stock
+            });
+        }
+
+        public async Task AddAsync(CreateProductDto dto)
+        {
+            var product = new Product
+            {
+                Name = dto.Name,
+                Price = dto.Price,
+                Stock = dto.Stock
+            };
+
+            await _repository.AddAsync(product);
+        }
+    }
+}
